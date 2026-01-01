baef719eee9b9fd368c53932acf6de2766d4382e
irfan <mohamedirfan7598@gmail.com>
2026-01-01 16:19:20 +0530

    Project Java

---
+++ src/org/Task1/ClassA.java
@@ -0,0 +1,27 @@
+package org.Task1;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+public class ClassA {
+	
+	public static void main(String[] args) {
+		
+		Map<String,Integer> m = new TreeMap();
+		
+		m.put("java", 10);
+		m.put("sql", 20);
+		m.put("oops", 30);
+		m.put("oracle", 40);
+		m.put("selenium", 50);
+		
+		System.out.println(m);
+		
+		
+		
+		
+	}
+
+
+
+}
